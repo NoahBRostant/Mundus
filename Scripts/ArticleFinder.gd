4d1722359a1e5cb16ac4650803ca31b5cb408900
NoahBRostant <noah.b.rostant@gmail.com>
2024-10-11 00:01:57 +0100

    Added Search Functionality

--- Scripts/ArticleFinder.gd
+++ Scripts/ArticleFinder.gd
@@ -1,6 +1,7 @@
 extends PanelContainer
 
 var article_item = preload("res://Scenes/ArticleNodeItem.tscn")
+var items
 
 func _ready():
 	var info = ConfigFile.new()
@@ -23,6 +24,7 @@ func _ready():
 				inst.core = true
 				%ArticleList.add_child(inst)
 			file_name = dir.get_next()
+	items = %ArticleList.get_children()
 
 
 func update_info(title:String, description:String, logo:Texture):
@@ -40,3 +42,15 @@ func conv_path_to_img(image_path):
 	var image_texture = ImageTexture.new()
 	image_texture.set_image(image)
 	return image_texture
+
+
+func _on_line_edit_text_changed(new_text: String) -> void:
+	if new_text != "":
+		for i in items:
+			if new_text.to_lower() in i.title.to_lower() or new_text.similarity(i.title) > 0.3 or new_text.similarity(i.description) > 0.2:
+				i.show()
+			else:
+				i.hide()
+	else:
+		for i in items:
+			i.show()
